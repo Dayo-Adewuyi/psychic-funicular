6da0bcf8f9d850443fb49ecd25d44b7d97462631
dayolovesruby <dayoadewuyi@protonmail.com>
2022-04-23 10:21:05 +0100

    first commit

---
+++ frontend/src/pages/About/index.jsx
@@ -0,0 +1,83 @@
+import classNames from "classnames";
+import { useStyles } from "./styles";
+
+import { useGlobalStyles } from "../../styles";
+import { Link } from "react-router-dom";
+import { useCallback, useContext , useState } from "react";
+import { AppContext } from '../../context/AppContext';
+
+
+const Home = () => {
+  const classes = useStyles();
+  const globalStyles = useGlobalStyles();
+  const { currentAccount, connectWallet , isStudent, addTeacher, changeChairman, removeTeacher,pauseContract,unPauseContract } = useContext(AppContext);
+  const[address, setAddress] = useState("")
+  const [chairman, setChairman] = useState("")
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  const handleAdd=async(e)=>{
+    e.preventDefault()
+    await addTeacher(address)
+  }
+  const handleRemove=async(e)=>{
+    e.preventDefault()
+    await removeTeacher(address)
+  }
+  const handleChair=async(e)=>{
+    e.preventDefault()
+    await changeChairman(address)
+  }
+
+  
+
+  return (
+    <main>
+      <section
+        className={classNames(
+          globalStyles.px,
+          classes.hero,
+          "bg-no-repeat flex flex-col pb-2"
+        )}
+      >
+
+        
+        (
+            <div className="col-md-9">
+            <div className="row mb-3">
+                <h3>Add Teacher</h3>
+            </div>
+            <div className="row">
+            <form class="row g-3" >
+                <div class="col-12">
+                    <label for="inputAddress" class="form-label">Address</label>
+                    <input type="text" class="form-control" id="inputAddress" placeholder="Enter Address" onChange={(e)=>setAddress(e.target.value)}/>
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary mr-7" onClick={handleAdd}>Add Teacher</button>
+                   {"     "}   {"     "}  <button type="submit" class=" pl-9 btn btn-warning"  onClick={handleRemove}>Remove Teacher</button>
+                </div>
+                </form>
+            </div>
+            <br/><br />
+            <div><label for="inputAddress" class="form-label">CHAIRMAN</label>
+                    <input type="text" class="form-control" id="inputAddress" placeholder="Enter Address" onChange={(e)=>setChairman(e.target.value)}/></div>
+                    <div><button type="submit" class="btn btn-primary mr-7" onClick={handleChair}>changeChairman</button></div>
+            <br/><br />
+           
+            <br/><br /><button  className="btn btn-lg btn-success" onClick={unPauseContract}>Restart Contract</button> <br/><br />
+            <button  className="btn btn-lg btn-danger" onClick={pauseContract}>Pause Contract</button> 
+           
+        </div>
+    
+        )
+        
+        
+      </section>
+     
+    </main>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/Election/index.jsx
@@ -0,0 +1,166 @@
+import classNames from "classnames";
+import { useStyles } from "./styles";
+import { ethers } from "ethers"
+
+
+import { useGlobalStyles } from "../../styles";
+import { Link } from "react-router-dom";
+import { useCallback, useContext, useState, useEffect} from "react";
+import { Typography } from "@mui/material";
+import { AppContext } from '../../context/AppContext';
+import Modal from 'react-bootstrap/Modal';
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; 
+import { contractAddress } from "../constants/constant";
+import abi from "../constants/abi.json";
+import { getProviderInfoFromChecksArray } from "web3modal";
+
+
+
+const Election = () => {
+  const classes = useStyles();
+  const globalStyles = useGlobalStyles();
+  const { currentAccount, connectWallet , isStudent , getProof , getCandidates, vote, startElection, endElection} = useContext(AppContext);
+  const [candidateId, setCandidateId] = useState(0)
+  const [show, setShow] = useState(false);
+  const[contenders, setContenders] = useState([]);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  console.log(contenders)
+
+
+  const handleVote = () => {
+  confirmAlert({
+  title: 'Confirm to submit',
+   message: 'Are you sure, You want to vote for this person',
+buttons: [
+       {
+         label: 'Yes',
+         onClick: () => alert('Click Yes')
+       },
+       {
+        label: 'No',
+         onClick: () => alert('Click No')
+       }
+     ]
+   });
+ }
+  
+  const fetch =async()=>{
+   
+
+    const contestants = await getCandidates()
+    setContenders(contestants)
+  console.log(contestants)
+     } 
+  
+     useEffect(()=>{
+      fetch()
+     })
+  
+
+  return (
+    <main>
+      <section
+        className={classNames(
+          globalStyles.px,
+          classes.hero,
+          "bg-no-repeat flex flex-col pb-2"
+        )}
+      >
+        {show ? (
+      
+              <Modal
+              size="lg"
+              show={show}
+              onHide={() => handleClose(false)}
+              aria-labelledby="example-modal-sizes-title-lg"
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="example-modal-sizes-title-lg">
+                  Candidates Manifesto
+                </Modal.Title>
+              </Modal.Header>
+  
+              <Modal.Body>
+                <h5 className="card-title">SAMUEL</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </Modal.Body>
+            </Modal>
+             
+        ) : (
+          ""
+        )}
+
+        {!isStudent ? (
+          <div className="container">
+            <div className="row">
+              <div className="d-flex justify-content-end">
+                <button className="btn btn-lg btn-success me-2">
+                  Start Election
+                </button>
+                <button className="btn btn-lg btn-danger me-2">
+                  End Election
+                </button>
+                <button className="btn btn-lg btn-primary me-2">
+                  Make Public
+                </button>
+                <button className="btn btn-lg btn-primary me-2">
+                  Make Private
+                </button>
+              </div>
+            </div>
+
+
+            <div className="row">
+            
+                <div className="col-4">
+                <div className="card">
+                  <img
+                    src="images/profile.jpg"
+                    className="card-img-top"
+                    alt="..."
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title"></h5>
+                    <span className="display-2"></span>
+                    <small>votes</small>
+                    <br></br>
+                    <button
+                      className="btn btn-primary btn-lg m-1 px-1"
+                      onClick={handleShow}
+                    >
+                      View Candidate
+                    </button>
+                    <button
+                      className="btn btn-success btn-lg m-1 px-1 "
+                      onClick={fetch}
+                    >
+                      Vote Candidate
+                    </button>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        ) : (
+          <div className="container">
+            <div className="row">
+              <div className="display-3">
+                This page is only accessible to the chairman and teachers
+              </div>
+            </div>
+          </div>
+        )}
+      </section>
+    </main>
+  );
+};
+
+export default Election;
